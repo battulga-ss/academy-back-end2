f1e48e5d1530c043afdfba9a313f7ac37d857c13
back-02-04 <back-02-04@Erxess-Mac-mini.local>
2025-10-14 19:38:52 +0800

    home work1

---
+++ script.js
@@ -0,0 +1,15 @@
+const a = window.prompt("nas");
+console.log(a);
+
+
+
+
+if( a < 13) {
+    console.log("hvvhed")
+}
+ else if ( a < 20) {
+console.log("zaluuchuud")
+}
+else if ( a < 60) {
+    console.log("nasandhursen")
+}
