bfc51cf039d6576ef9ae9e451578995f9415c68e
back-02-04 <back-02-04@Erxess-Mac-mini.local>
2025-10-27 20:18:07 +0800

    ajillahgui bga

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
- 
-    const correctUsername = "admin@erxes.io";
-    const correctPassword = "12345";
 
+
+const dice = document.getElementById("button1")
+const RPS = document.getElementById("button2")
+const GuessNumber = document.getElementById("button3")
+const GuessWord = document.getElementById("button4")
+const exit = document.getElementById("button5")
+const togloh = document.getElementById("RunBtn")
+const result = document.getElementById("result")
+
+
+
+//dice
+
+const shoo1 = Math.floor(Math.random() * 6) + 1
+const shoo2 = Math.floor(Math.random() * 6) + 1
+
+
+
+
+const RunBtn = () => {
+const result = shoo1 + shoo2
+  const shoo1Element = document.getElementById("shoo1")
+  const shoo2Element = document.getElementById("shoo2")
+
+  const shoo1 = shoo1Element.value
+  const shoo2 = shoo2Element.value
+listElement.innerHTML = ""
+listElement.innerHTML = `<li><p>${result}</p></li>`;
+
+const li = document.createElement("li");
+
+
+
+
+if (result === 8 && 11 ){ 
+
+  <li><p>${"ta hojloo"}</p></li>
+
+   }else {
     
-    document.getElementById("loginBtn").onclick = () => {
-      const user = document.getElementById("username").value;
-      const pass = document.getElementById("password").value;
-      const result = document.getElementById("result");
+      <li><p>${"ta hojigdoo"}</p></li>
 
-      if (user === correctUsername && pass === correctPassword) {
-        window.alert("Login successful!");
-        
-      } else {
-         window.alert("Invalid username or password!")
-        
-      }
-    };
+    }
+ 
+}
+
+RunBtn.addEventListener("click",result)
